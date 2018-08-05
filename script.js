637f9333ba8ff3fdb089ac6226ac14e8d7035c5b
oluwashola1100 <oluwasholatemitayo@gmail.com>
2018-08-05 23:14:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("open").addEventListener("click", function() {
+    document.querySelector("ul").style.display = "block";
+});
+document.querySelector(".close").addEventListener("click", function() {
+    document.querySelector("ul").style.display = "none";
+});
